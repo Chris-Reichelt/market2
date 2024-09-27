e876b44d11006e355a0088320e5cb27d0835f419
Chris Reichelt <reicheltcm@gmail.com>
2024-09-27 20:24:00 +0000

    v0.94 revert

--- db_manager.py
+++ db_manager.py
@@ -2,7 +2,7 @@ import sqlite3
 from utils import hash_password  # Import from utils.py
 
 def get_connection():
-    conn = sqlite3.connect('market2.db')
+    conn = sqlite3.connect('market.db')
     conn.row_factory = sqlite3.Row
     return conn
 
@@ -67,42 +67,11 @@ def get_user(username):
     conn.close()
     return user
 
-def get_user_by_username(username):
+def add_user(username, password, user_type):
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
-    user = cursor.fetchone()
-    conn.close()
-    return user
-
-
-
-def add_bid(username, company_name, bid_amount):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute('''
-        INSERT INTO bids (username, company_name, bid_amount)
-        VALUES (?, ?, ?)
-    ''', (username, company_name, bid_amount))
-    conn.commit()
-    conn.close()
-
-def get_company_list():
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT name FROM companies")
-    companies = [row['name'] for row in cursor.fetchall()]
-    conn.close()
-    return companies
-
-
-
-
-def add_user(username, password, email, user_type):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (username, password, email, user_type) VALUES (?, ?, ?, ?)",
-                   (username, password, email, user_type))
+    cursor.execute("INSERT INTO users (username, password, user_type) VALUES (?, ?, ?)",
+                   (username, password, user_type))
     conn.commit()
     conn.close()
 
--- utils.py
+++ utils.py
@@ -4,30 +4,6 @@ import sendgrid
 from sendgrid.helpers.mail import Mail
 import streamlit as st
 
-def send_email(to_email, subject, content):
-    sendgrid_api_key = st.secrets["sendgrid"]["api_key"]
-    from_email = st.secrets["sendgrid"]["from_email"]
-
-    sg = sendgrid.SendGridAPIClient(api_key=sendgrid_api_key)
-    message = Mail(
-        from_email=from_email,
-        to_emails=to_email,
-        subject=subject,
-        html_content=content
-    )
-
-    try:
-        response = sg.send(message)
-        if response.status_code == 202:
-            return True
-        else:
-            st.error(f"Failed to send email. Status code: {response.status_code}")
-            return False
-    except Exception as e:
-        st.error(f"An error occurred while sending email: {e}")
-        return False
-
-
 def hash_password(password):
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
